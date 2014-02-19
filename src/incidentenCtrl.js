91682a0257b13e4d056616c97c56488193b4da08
Steven Ottens <github@minst.net>
2014-02-19 14:12:56 +0100

    projecten nieuwe stijl

--- src/incidentenCtrl.js
+++ src/incidentenCtrl.js
@@ -5,10 +5,17 @@
 /*
  * Deze angular control gaat over de lijst met incidenten in /incidenten
  */
-icm.controller('IncidentenCtrl' ,['$scope', function($scope){
+icm.controller('IncidentenCtrl' ,['$scope', 'Core', function($scope, Core){
     console.log('creating IncidentenCtrl');
-    $scope.projectStore = {};
-  
+    
+    $scope.project = Core.project();
+    var store = Core.projectStore();
+    $scope.projecten = Core.projects();
+    store.bind('datachange', function () {
+        $scope.$apply(function(){
+            $scope.projecten = Core.projects();
+        })
+    })
 
 }]);
 
