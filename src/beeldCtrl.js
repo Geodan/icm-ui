9d37486e53d5cb63b15688d3f3e746ef3623325d
stvno <github@minst.net>
2014-02-25 11:57:06 +0100

    safeguard voor verwijderen

--- src/beeldCtrl.js
+++ src/beeldCtrl.js
@@ -19,6 +19,7 @@ icm.controller('BeeldCtrl', ['$scope', '$stateParams', 'Beelden', 'Core', 'Utils
          $scope.items = Utils.filter(Core.project().items(), $scope.currentBeeld.beeld);
          _($scope.currentBeeld.beeldonderdeel).each(function(d){
             if(d.isedit === undefined) d.isedit = false;
+            if(d.zeker === undefined) d.zeker = true;
             var item = _($scope.items).filter(function(b){
                 return b.data('beeldonderdeel') == d.id
             })
@@ -52,6 +53,10 @@ icm.controller('BeeldCtrl', ['$scope', '$stateParams', 'Beelden', 'Core', 'Utils
             }
             else {
                 //er is nog geen item, we gaan een nieuwe maken
+                if(!onderdeel.contentedit) {
+                    this.onderdeel.isedit = !isedit;
+                    return false //er is geen content dus ook niet gaan syncen
+                }
                 var id = $scope.beeldType + '_' + onderdeel.id;
                 var item = Core.project().items({_id:id})
                     .data('beeld',$scope.beeldType)
@@ -70,9 +75,17 @@ icm.controller('BeeldCtrl', ['$scope', '$stateParams', 'Beelden', 'Core', 'Utils
         this.onderdeel.isedit = !isedit;
     }
     $scope.cancelEdit = function() {
-
-        this.onderdeel.isedit = false;
-        this.onderdeel.content = this.onderdeel.oldVersion;
+        if(this.onderdeel.zeker) {
+            this.onderdeel.zeker = false;
+        }
+        else {
+            this.onderdeel.zeker = true;
+            this.onderdeel.isedit = false;
+            this.onderdeel.content = this.onderdeel.oldVersion;
+        }
+    }
+    $scope.onzeker = function() {
+        this.onderdeel.zeker = true;
     }
 
 }])
