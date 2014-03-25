766ddad8c86a747980131f5b502c9de9bb697506
geodan <tom.van.tilburg@gmail.com>
2014-03-25 11:19:39 +0100

    Nicer syncprogressbars

--- src/controllers/syncCtrl.js
+++ src/controllers/syncCtrl.js
@@ -1,23 +1,43 @@
 icm.controller('SyncCtrl', ['$scope', '$stateParams', '$location', 'Core', 'Utils', function  ($scope, $stateParams, $location, Core, Utils) {
-  $scope.maxprojects = 100;
+  $scope.maxprojects = 100; //Total amount to receive
   $scope.maxusers = 100;
+  $scope.curprojects = 0; //Amount we already have
+  $scope.curusers = 0;
+  $scope.queuedProjects = 0;
+  $scope.queuedUsers = 0;
+  $scope.syncedprojects = 100;
+  $scope.syncedusers = 100;
   
   var update = function(){
       $scope.$apply(function(){
-          $scope.maxprojects = Core.projectStore().syncinfo.numToReceive;
+          $scope.curprojects = Core.projects().length; //Increasing, current amount
+          $scope.curusers = Core.users().length;
+          $scope.maxprojects = Core.projectStore().syncinfo.numToReceive; //stays the same
           $scope.maxusers = Core.userStore().syncinfo.numToReceive;
-          $scope.syncedprojects =  100 - 100 * (Core.projectStore().syncinfo.toReceive.length / Core.projectStore().syncinfo.numToReceive);
-          $scope.syncedusers =  100 - 100 * (Core.userStore().syncinfo.toReceive.length / Core.userStore().syncinfo.numToReceive);
+          $scope.queuedProjects = projectstore.syncinfo.toReceive.length; //decreasing, left to receive
+          $scope.queuedUsers = userstore.syncinfo.toReceive.length;
+          
+          var totalprojects = $scope.curprojects + $scope.queuedProjects;
+          var totalusers = $scope.curusers + $scope.queuedUsers;
+          
+          $scope.syncedprojects =  100 * ($scope.curprojects / totalprojects);
+          $scope.syncedusers =  100 * ($scope.curusers / totalusers);
       });
   };
 
-  //Update de items na een datachange van de itemStore
+  
   var projectstore = Core.projectStore();
+  projectstore.loaded.then(function(){
+      $scope.curprojects = Core.projects().length;
+  });
   projectstore.bind('datachange', function () {
       window.setTimeout(update,10);
   });
   
   var userstore = Core.userStore();
+  userstore.loaded.then(function(){
+      $scope.curprojects = Core.projects().length;
+  });
   userstore.bind('datachange', function () {
       window.setTimeout(update,10);
   });
