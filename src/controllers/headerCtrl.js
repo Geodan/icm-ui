3d71fe35005b9b3b1a05e51e107a9d2f6e5aca21
geodan <tom.van.tilburg@gmail.com>
2014-04-23 12:09:24 +0200

    Added print button (export to lcms)

--- src/controllers/headerCtrl.js
+++ src/controllers/headerCtrl.js
@@ -16,6 +16,16 @@ icm.controller('HeaderCtrl', ['$scope', '$location', 'Core', 'Utils', function  
         $location.path('/login');
     };
 
+	$scope.print = function(){
+		window.print();
+		//workaround for Chrome bug - https://code.google.com/p/chromium/issues/detail?id=141633
+		if (window.stop) {
+			location.reload(); //triggering unload (e.g. reloading the page) makes the print dialog appear
+			window.stop(); //immediately stop reloading
+		}
+		return false;
+	}
+	
     function clearProject() {
         Core.peer().data('activeproject', ' ').sync();
         $scope.data.incident = '';
